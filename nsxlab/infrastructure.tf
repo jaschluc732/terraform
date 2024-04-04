e285d9cbe05e8b91f806d00a98dd9fec33431d1d
Jaschluckbier <jack.schluckbier@arista.com>
2024-04-04 09:58:27 -0500

    edited infra

--- nsxlab/infrastructure.tf
+++ nsxlab/infrastructure.tf
@@ -1,28 +1,21 @@
 data "nsxt_policy_edge_cluster" "CLUSTER1" {
-    display_name = "Cluster-1"
+    display_name = "Prod-Cluster"
 }
 
 data "nsxt_policy_transport_zone" "TZ-OVERLAY" {
-    display_name = "TZ-Overlay"
+    display_name = "tz-overlay"
 }
 
 data "nsxt_policy_transport_zone" "TZ-VLAN" {
-    display_name = "TZ-VLAN"
+    display_name = "tz-vlan"
 }
 
 data "nsxt_policy_edge_node" "edge1" {
     edge_cluster_path = data.nsxt_policy_edge_cluster.CLUSTER1.path
-    display_name = "edge1"
+    display_name = "nsx-edge1"
 }
 
 data "nsxt_policy_edge_node" "edge2" {
     edge_cluster_path = data.nsxt_policy_edge_cluster.CLUSTER1.path
-    display_name = "edge2"
-}
-
-#Define Distributed DHCP server
-resource "nsxt_policy_dhcp_server" "demo_dhcp_server" {
-  display_name = "demo_dhcp"
-  description  = "DHCP Server for lab assignment"
-  server_addresses = []
+    display_name = "nsx-edge2"
 }
